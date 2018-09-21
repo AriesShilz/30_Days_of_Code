08bc1b71310f1e7b76fbd43543340890c59b1997
AriesShilz <ariesshilz@gmail.com>
2018-09-21 22:13:50 +0530

    initial commit

---
+++ day_1_data_types.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+    
+    // Declare second integer, double, and String variables.
+    int j;
+    double k;
+    string l;
+    // Read and save an integer, double, and String to your variables.
+    cin>>j>>k;
+    cin.ignore();
+    getline(cin,l);
+    // Print the sum of both integer variables on a new line.
+    cout<<i+j<<endl;
+    // Print the sum of the double variables on a new line.
+    cout<<fixed<<setprecision(1)<<d+k<<endl;
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    cout<<s+l<<endl;
+    return 0;
